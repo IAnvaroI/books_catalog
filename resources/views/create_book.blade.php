43de970ba3cae22b07d972010e015dc2bdaa6b1a
Andriy.Romanov <anvaro2406@gmail.com>
2021-04-21 15:04:47 +0300

    Initial commit

---
+++ resources/views/create_book.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Books</title>
+
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-4 text-primary">jkjsjdsjd</div>
+            <div class="col-sm-4 text-danger">jkjsjdsjd</div>
+            <div class="col-sm-4 text-success">jkjsjdsjd</div>
+        </div>
+        <div class="row text-center">
+            <div class="col-sm-4 text-primary">jkjsjdsjd</div>
+            <div class="col-sm-4 text-danger">jkjsjdsjd</div>
+            <div class="col-sm-4 text-success">jkjsjdsjd</div>
+        </div>
+    </div>
+</body>
+</html>
